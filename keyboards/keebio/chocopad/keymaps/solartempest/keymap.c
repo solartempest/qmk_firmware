54ced4bf42029184e7998d92c116e7a913fc3d25
solartempest <82990256+solartempest@users.noreply.github.com>
2024-01-22 00:42:11 -0500

    Layer lighting working

--- keyboards/keebio/chocopad/keymaps/solartempest/keymap.c
+++ keyboards/keebio/chocopad/keymaps/solartempest/keymap.c
@@ -2,11 +2,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-<<<<<<< HEAD
 uint8_t led_min = 0;
 uint8_t led_max = RGB_MATRIX_LED_COUNT;
-=======
->>>>>>> dcb7add53037cf0f79c5b7b7e73b66b24f8b3861
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -24,7 +21,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
-<<<<<<< HEAD
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     HSV hsv = {HSV_SPRINGGREEN};
 	if (layer_state_cmp(layer_state, 1)) {
@@ -35,68 +31,6 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         hsv = (HSV){HSV_MAGENTA};
     } else {
         hsv = (HSV){HSV_SPRINGGREEN};
-=======
-/*led_config_t g_led_config = { {
-  // Key Matrix to LED Index
-	{	NO_LED,	0,	1,	NO_LED	},
-	{	2,	3,	4,	5	},
-	{	6,	7,	8,	9	},
-	{	10,	11,	12,	13	},
-	{	14,	15,	16,	17	},
-	{	NO_LED,	18,	19,	NO_LED	}
-}, {
-  // LED Index to Physical Position
-  {37,0},{187,0},{0,0},{74,0},{149,0},{224,0},{0,21},{74,21},{149,21},{224,21},{0,43},{74,43},{149,43},{224,43},{0,64},{74,64},{149,64},{224,64},{37,64},{187,64}
-}, {
-  // LED Index to Flag
-  2,2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,2,2
-} };*/
-
-
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    for (uint8_t i = led_min; i < led_max; i++) {
-        switch(get_highest_layer(layer_state|default_layer_state)) {
-            case 2:
-                rgb_matrix_set_color(i, RGB_BLUE);
-                break;
-            case 1:
-                rgb_matrix_set_color(i, RGB_SPRINGGREEN);
-                break;
-            default:
-                break;
-        }
-    }
-    return false;
-} //Works for all LEDs set to these colours only
-
-/*bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    if (get_highest_layer(layer_state) > 0) {
-        uint8_t layer = get_highest_layer(layer_state);
-
-        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
-            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
-                uint8_t index = g_led_config.matrix_co[row][col];
-
-                if (index >= led_min && index < led_max && index != NO_LED &&
-                keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
-                    rgb_matrix_set_color(index, RGB_SPRINGGREEN );
-                }
-            }
-        }
-    }
-    return false;
-}*/	//Works for keys only, not underglow
-
-
-/*bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    HSV hsv = {106, 255, 255};	//spring green
-
-	hsv = {HSV_AZURE};
-    if (layer_state_cmp(layer_state, 2)) {
-        hsv = {132, 102, 255};	//azure
-    } else {
-        hsv = {30, 218, 218};	//goldenrod
->>>>>>> dcb7add53037cf0f79c5b7b7e73b66b24f8b3861
     }
 
     if (hsv.v > rgb_matrix_get_val()) {
@@ -110,11 +44,4 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         }
     }
     return false;
-<<<<<<< HEAD
 }
-=======
-}*/
-
-
-
->>>>>>> dcb7add53037cf0f79c5b7b7e73b66b24f8b3861
--- keyboards/keebio/chocopad/keymaps/solartempest/rules.mk
+++ keyboards/keebio/chocopad/keymaps/solartempest/rules.mk
@@ -1,6 +1,2 @@
 VIA_ENABLE = yes
-<<<<<<< HEAD
 LTO_ENABLE = yes
-=======
-LTO_ENABLE = yes
->>>>>>> dcb7add53037cf0f79c5b7b7e73b66b24f8b3861
